2b322feef7bbcfe11468f36b8b473032cd15daa7
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-03-04 11:17:29 +0100

    add example for surface calculation

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ require __DIR__ . '/vendor/autoload.php';
 use Acme\Shapes as Shapes;
 use Acme\ShapeCalculator as ShapeCalculator;
 
+/*
+ * Area Calculation
+ */
 $shapes = array(
     new Shapes\Square(1),
     new Shapes\Circle(1)
@@ -16,11 +19,27 @@ $outputter = new ShapeCalculator\Outputter($areaCalculator);
 
 echo $outputter->json();
 
+/*
+ * Volume Calculation
+ */
+$shapes = array(
+    new Shapes\Cube(1, 2, 3)
+);
+
+$volumeCalculator = new ShapeCalculator\VolumeCalculator($shapes);
+
+$outputter = new ShapeCalculator\Outputter($volumeCalculator);
+
+echo $outputter->json();
+
+/*
+ * Surface Calculation
+ */
 $shapes = array(
     new Shapes\Cube(1, 1, 1)
 );
 
-$volumeCalculator = new ShapeCalculator\VolumeCalculator($shapes);
+$volumeCalculator = new ShapeCalculator\SurfaceAreaCalculator($shapes);
 
 $outputter = new ShapeCalculator\Outputter($volumeCalculator);
 
