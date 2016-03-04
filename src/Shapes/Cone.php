22f3c4b4e4be4eb9ff102f3837441c83c4b13bf5
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-03-04 17:10:59 +0100

    cleans up cone class

--- src/Shapes/Cone.php
+++ src/Shapes/Cone.php
@@ -20,8 +20,8 @@ class Cone implements SolidInterface
     /**
      * Cone constructor.
      *
-     * @param $radius
-     * @param $height
+     * @param float|int $radius
+     * @param float|int $height
      */
     public function __construct($radius, $height)
     {
