a14b66116c2770c4ecee9c6ef324414814e0bf9f
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-03-04 12:41:18 +0100

    add cylinder shape

---
+++ src/Shapes/Cylinder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Acme\Shapes;
+
+use Acme\Shapes\Contracts\ThreeDimensionalInterface;
+
+class Cylinder implements ThreeDimensionalInterface
+{
+
+    /**
+     * @var int|float
+     */
+    private $length;
+
+    /**
+     * @var int|float
+     */
+    private $radius;
+
+    /**
+     * Cylinder constructor.
+     *
+     * @param $radius
+     * @param $length
+     */
+    public function __construct($radius, $length)
+    {
+        $this->length = $length;
+        $this->radius = $radius;
+    }
+
+    /**
+     * @return float
+     */
+    public function volume()
+    {
+        return pi() * pow($this->radius, 2) * $this->length;
+    }
+
+    /**
+     * @return float
+     */
+    public function surfaceArea()
+    {
+        return 2 * pi() * 2 * $this->radius + pi() * pow($this->radius, 2) * $this->length;
+    }
+}
