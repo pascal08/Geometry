ca90267e83bd2be3c74439532aba701ccc6045c0
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-03-04 12:41:30 +0100

    clean up examples

--- index.php
+++ index.php
@@ -6,41 +6,186 @@ use Acme\Shapes as Shapes;
 use Acme\ShapeCalculator as ShapeCalculator;
 
 /*
- * Area Calculation
+ * Circle
  */
-$shapes = array(
-    new Shapes\Square(1),
-    new Shapes\Circle(1)
+
+echo 'Circle: (radius = 2)';
+echo PHP_EOL;
+
+// Area
+echo ' Area: ';
+
+$circles = array(
+    new Shapes\Circle(2)
 );
 
-$areaCalculator = new ShapeCalculator\AreaCalculator($shapes);
+$areaCalculator = new ShapeCalculator\AreaCalculator($circles);
 
 $outputter = new ShapeCalculator\Outputter($areaCalculator);
 
 echo $outputter->json();
+echo PHP_EOL;
 
 /*
- * Volume Calculation
+ * Cube
  */
-$shapes = array(
-    new Shapes\Cube(1, 2, 3)
+
+echo 'Cube: (length = 3)';
+echo PHP_EOL;
+
+// Volume
+echo ' Volume: ';
+
+$cubes = array(
+    new Shapes\Cube(3)
 );
 
-$volumeCalculator = new ShapeCalculator\VolumeCalculator($shapes);
+$volumeCalculator = new ShapeCalculator\VolumeCalculator($cubes);
 
 $outputter = new ShapeCalculator\Outputter($volumeCalculator);
 
 echo $outputter->json();
+echo PHP_EOL;
 
-/*
- * Surface Calculation
- */
-$shapes = array(
-    new Shapes\Cube(1, 1, 1)
+// Surface Area
+echo ' Surface Area: ';
+
+$cubes = array(
+    new Shapes\Cube(3)
 );
 
-$volumeCalculator = new ShapeCalculator\SurfaceAreaCalculator($shapes);
+$volumeCalculator = new ShapeCalculator\SurfaceAreaCalculator($cubes);
 
 $outputter = new ShapeCalculator\Outputter($volumeCalculator);
 
 echo $outputter->json();
+echo PHP_EOL;
+
+/*
+ * Cylinder
+ */
+
+echo 'Cylinder: (radius = 2, length = 3)';
+echo PHP_EOL;
+
+// Volume
+echo ' Volume: ';
+
+$cylinders = array(
+    new Shapes\Cylinder(2, 3)
+);
+
+$volumeCalculator = new ShapeCalculator\VolumeCalculator($cylinders);
+
+$outputter = new ShapeCalculator\Outputter($volumeCalculator);
+
+echo $outputter->json();
+echo PHP_EOL;
+
+// Surface Area
+echo ' Surface Area: ';
+
+$cylinders = array(
+    new Shapes\Cylinder(2, 3)
+);
+
+$volumeCalculator = new ShapeCalculator\SurfaceAreaCalculator($cylinders);
+
+$outputter = new ShapeCalculator\Outputter($volumeCalculator);
+
+echo $outputter->json();
+echo PHP_EOL;
+
+
+/*
+ *  Rectangular Prism
+ */
+
+echo 'Rectangular Prism: (width = 1, depth = 2, height = 3)';
+echo PHP_EOL;
+
+// Volume
+echo ' Volume: ';
+
+$rectangularPrisms = array(
+    new Shapes\RectangularPrism(1, 2, 3)
+);
+
+$volumeCalculator = new ShapeCalculator\VolumeCalculator($rectangularPrisms);
+
+$outputter = new ShapeCalculator\Outputter($volumeCalculator);
+
+echo $outputter->json();
+echo PHP_EOL;
+
+// Surface Area
+echo ' Surface Area: ';
+
+$rectangularPrisms = array(
+    new Shapes\RectangularPrism(1, 2, 3)
+);
+
+$volumeCalculator = new ShapeCalculator\SurfaceAreaCalculator($rectangularPrisms);
+
+$outputter = new ShapeCalculator\Outputter($volumeCalculator);
+
+echo $outputter->json();
+echo PHP_EOL;
+
+
+/*
+ *  Sphere
+ */
+
+echo 'Sphere: (radius = 2)';
+echo PHP_EOL;
+
+// Volume
+echo ' Volume: ';
+
+$spheres = array(
+    new Shapes\Sphere(2)
+);
+
+$areaCalculator = new ShapeCalculator\VolumeCalculator($spheres);
+
+$outputter = new ShapeCalculator\Outputter($areaCalculator);
+
+echo $outputter->json();
+echo PHP_EOL;
+
+// Surface Area
+echo ' Surface Area: ';
+
+$spheres = array(
+    new Shapes\Sphere(2)
+);
+
+$areaCalculator = new ShapeCalculator\SurfaceAreaCalculator($spheres);
+
+$outputter = new ShapeCalculator\Outputter($areaCalculator);
+
+echo $outputter->json();
+echo PHP_EOL;
+
+
+/*
+ *  Square
+ */
+
+echo 'Square: (length = 2)';
+echo PHP_EOL;
+
+// Area
+echo ' Area: ';
+
+$squares = array(
+    new Shapes\Square(2)
+);
+
+$areaCalculator = new ShapeCalculator\AreaCalculator($squares);
+
+$outputter = new ShapeCalculator\Outputter($areaCalculator);
+
+echo $outputter->json();
+echo PHP_EOL;
