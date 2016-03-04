d0cae407dcb5f44f979d6caaf4adb2cb2bf159ba
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-03-04 13:11:49 +0100

    add cone shape

---
+++ src/Shapes/Cone.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Acme\Shapes;
+
+use Acme\Shapes\Contracts\ThreeDimensionalInterface;
+
+class Cone implements ThreeDimensionalInterface
+{
+
+    /**
+     * @var int|float
+     */
+    private $height;
+
+    /**
+     * @var int|float
+     */
+    private $radius;
+
+    /**
+     * Cone constructor.
+     *
+     * @param $radius
+     * @param $height
+     */
+    public function __construct($radius, $height)
+    {
+        $this->height = $height;
+        $this->radius = $radius;
+    }
+
+    /**
+     * @return float
+     */
+    public function volume()
+    {
+        return (1/3) * pi() * pow($this->radius, 2) * $this->height;
+    }
+
+    /**
+     * @return float
+     */
+    public function surfaceArea()
+    {
+        return pi() * $this->radius * ($this->radius + sqrt(pow($this->radius, 2) + pow($this->height, 2)));
+    }
+}
