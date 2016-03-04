52b72c271bfb744dafae0be8b8c31f7fc51e491d
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-03-04 16:02:31 +0100

    adds tests for area calculation

--- tests/AreaCalculatorTest.php
+++ tests/AreaCalculatorTest.php
@@ -8,19 +8,73 @@ use Acme\ShapeCalculator as ShapeCalculator;
 class AreaCalculatorTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected $delta;
+
+    protected function setUp()
+    {
+        $this->delta = 0.0001;
+    }
+
     public function testCircle()
     {
-        $circles = array(
-            new Shapes\Circle(2)
+        $circle = array(
+            new Shapes\Circle(5.57)
         );
 
-        $areaCalculator = new ShapeCalculator\AreaCalculator($circles);
+        $areaCalculator = new ShapeCalculator\AreaCalculator($circle);
 
         $area = $areaCalculator->calculate();
 
-        $assertValue = 12.5664;
+        $assertValue = 97.4676;
 
-        $this->assertEquals($area, $assertValue, '', 0.0001);
+        $this->assertEquals($area, $assertValue, '', $this->delta);
     }
 
+    public function testRectangle()
+    {
+        $rectangle = array(
+            new Shapes\Rectangle(7.45, 13.18)
+        );
+
+        $areaCalculator = new ShapeCalculator\AreaCalculator($rectangle);
+
+        $area = $areaCalculator->calculate();
+
+        $assertValue = 98.191;
+
+        $this->assertEquals($area, $assertValue, '', $this->delta);
+    }
+
+    public function testSquare()
+    {
+        $square = array(
+            new Shapes\Square(14.20)
+        );
+
+        $areaCalculator = new ShapeCalculator\AreaCalculator($square);
+
+        $area = $areaCalculator->calculate();
+
+        $assertValue = 201.64;
+
+        $this->assertEquals($area, $assertValue, '', $this->delta);
+    }
+
+
+    public function testMultipleShapes()
+    {
+        $shapes = array(
+            new Shapes\Circle(8.19),
+            new Shapes\Rectangle(7.33, 7.91),
+            new Shapes\Square(5.44),
+        );
+
+        $areaCalculator = new ShapeCalculator\AreaCalculator($shapes);
+
+        $area = $areaCalculator->calculate();
+
+        $assertValue = 298.2997;
+
+        $this->assertEquals($area, $assertValue, '', $this->delta);
+    }
 }
